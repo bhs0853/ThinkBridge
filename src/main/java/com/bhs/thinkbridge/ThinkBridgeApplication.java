082c61f21415615fdcbd2ac431e1e64b546aa75b
Hema Sai Bonda <hemasaibonda1@gmail.com>
2024-11-30 15:20:51 +0530

    Initial Setup

---
+++ src/main/java/com/bhs/thinkbridge/ThinkBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.bhs.thinkbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhs/thinkbridge/ThinkBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhs.thinkbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
